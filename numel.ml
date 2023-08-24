ee18d727c9ead5c7ff77530ff0b0c8f6a325efa0
wolfep15 <patrick.wolfe@tufts.edu>
2023-08-23 22:07:31 -0400

    Add correct window size and run until user input

--- numel.ml
+++ numel.ml
@@ -1,5 +1,10 @@
+open Stdio
+
 let () = begin 
     print_endline "Hello, numel!";
     Graphics.open_graph "";
+    Graphics.set_window_title "numel";
+    Graphics.resize_window 240 160;
+    Stdio.In_channel.input_char stdin |> ignore; 
     print_endline "Goodbye, numel.";
 end
